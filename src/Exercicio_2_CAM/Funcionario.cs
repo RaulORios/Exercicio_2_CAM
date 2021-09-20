180bd54a171e4891dbf24553984bf81da7afffed
RaulORios <raul.orios89@outlook.com>
2021-09-20 14:59:24 -0300

    Arwuivos adicionados

---
+++ src/Exercicio_2_CAM/Funcionario.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio_2_CAM
+{
+    class Funcionario
+    {
+        public string Nome;
+        public double SalarioBruto;
+        public double Imposto;
+
+        public double SalarioLiquido()
+        {
+            return SalarioBruto - Imposto;
+        }
+
+        public void AumentarSalario(double impostos)
+        {
+            SalarioBruto += ((SalarioBruto * impostos) / 100);
+        }
+
+        public override string ToString()
+        {
+            return ($"{Nome}, $ {SalarioLiquido().ToString("F2",CultureInfo.InvariantCulture)}"); 
+        }
+
+
+
+
+    }
+}
---
+++ src/Exercicio_2_CAM/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio_2_CAM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario f = new Funcionario();
+
+            Console.Write("Nome: ");
+            f.Nome = Console.ReadLine();
+            Console.Write("Sálario Bruto: ");
+            f.SalarioBruto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Imposto: ");
+            f.Imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine();
+            Console.WriteLine($"Funcionario: {f}");
+
+            Console.WriteLine();
+            Console.Write("Digite a procentagem para aumentar o sálario: ");
+            double porc = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            f.AumentarSalario(porc);
+
+            Console.WriteLine();
+            Console.WriteLine($"Dados atualizados: {f}");
+
+            Console.ReadKey();
+
+        }
+    }
+}
